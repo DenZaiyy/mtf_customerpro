ab7e39f234dac0487f9e7f4f291ea4621b286057
DenZaiyy <denzaiyy@outlook.fr>
2025-03-06 15:54:28 +0100

    feat: translations

---
+++ translations/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mtf_customerpro}prestashop>mtf_customerpro_3f5da79faeefefa33c81defc2f00cb5f'] = 'Votre compte professionnel a été créé avec succès. Il est actuellement en attente de validation. Vous recevrez un email dès qu\'il sera activé.';
+$_MODULE['<{mtf_customerpro}prestashop>mtf_customerpro_aa618402619b2a965fbff8316524a14e'] = 'Votre compte professionnel a été activé';
+$_MODULE['<{mtf_customerpro}prestashop>mtf_customerpro_ff54a6edf3f0fac6bea3aea88dc3c53f'] = 'Nouveau compte professionnel';
+$_MODULE['<{mtf_customerpro}prestashop>mtf_customerpro_2a20f35a8b25df538eb103f298524e1f'] = 'Confirmation de votre inscription';
